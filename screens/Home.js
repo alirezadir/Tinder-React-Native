5f84006edd5000d302f36c936c2cd700816dff27
Alireza Dirafzoon <adirafz@ncsu.edu>
2018-10-16 22:04:55 -0700

    added redux

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from '../styles'
+import { connect } from 'react-redux';
+import { login } from '../redux/actions'
 
 import { 
   Text, 
@@ -9,15 +11,23 @@ import {
 class Home extends React.Component {
   state = {}
 
-  componentWillMount() {}
+  componentWillMount() {
+    this.props.dispatch(login("whats up"))
+  }
 
   render() {
     return (
      <View>
-      <Text>Home</Text>
+      <Text>{this.props.user}</Text>
      </View>
     )
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(Home);
